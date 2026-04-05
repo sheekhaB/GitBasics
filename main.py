734c5a92799a0fd07c90e80e8e21cc9d6483674f
Sheekha <sheekhab25@gmail.com>
2026-04-05 22:49:49 +0530

    inported library

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
+import numpy as np
 
 # my first project
 response = requests.get("https://api.github.com")
 print("GitHub API status:", response.status_code)
 print("You're set up correctly!")
+
+
